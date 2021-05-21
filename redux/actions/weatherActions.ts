705b6c7465a424d9c9608b875f49b725ef8e14cb
Tim Corley <tcorley215@gmail.com>
2021-05-21 13:37:29 -0400

    initial config
    adding redux store

---
+++ redux/actions/weatherActions.ts
@@ -0,0 +1,50 @@
+import { ThunkAction } from "redux-thunk";
+import { RootState } from "../store";
+import {
+  WeatherAction,
+  WeatherData,
+  WeatherError,
+  GET_WEATHER,
+  SET_LOADING,
+  SET_ERROR,
+} from "../types";
+
+export const getWeather = (
+  city: string
+): ThunkAction<void, RootState, null, WeatherAction> => {
+  return async (dispatch) => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+      );
+
+      if (!res.ok) {
+        const resData: WeatherError = await res.json();
+        throw new Error(resData.message);
+      }
+      const resData: WeatherData = await res.json();
+      dispatch({
+        type: GET_WEATHER,
+        payload: resData,
+      });
+    } catch (err) {
+      dispatch({
+        type: SET_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
+
+export const setLoading = (): WeatherAction => {
+  return {
+    type: SET_LOADING,
+  };
+};
+
+export const setError = (): WeatherAction => {
+  return {
+    type: SET_ERROR,
+    payload: "",
+  };
+};
